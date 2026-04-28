193891fd89c1bca17dd19ee8be727016b3723961
Reis Saigyouji <olegride22@gmail.com>
2026-04-28 15:05:18 +0200

    Made power upgrades work

---
+++ index.js
@@ -0,0 +1,39 @@
+var count = 0;
+var power = 1;
+var price = 20;
+const counter = document.getElementById('counter');
+const clickbtn = document.getElementById('click');
+const upbtn = document.getElementById('upgrade');
+const cost = document.getElementById('cost');
+
+clickbtn.onclick = function() {
+    count += power;
+    counter.innerText = count;
+}
+
+upbtn.onclick = function() {
+    if (count >= price && price === 20) {
+        power *= 2;
+        price = 100;
+        cost.innerText = 'Upgrade cost: 100';
+    }
+    else if (count >= price && price === 100) {
+        power *= 2;
+        price = 300;
+        cost.innerText = 'Upgrade cost: 300';
+    }
+    else if (count >= price && price === 300) {
+        power *= 2;
+        price = 600;
+        cost.innerText = 'Upgrade cost: 600';
+    }
+    else if (count >= price && price === 600) {
+        power *= 2;
+        price = 999;
+        cost.innerText = 'Upgrade cost: 999';
+    }
+    else if (count >= price && price === 999) {
+        power *= 2;
+        cost.innerText = 'Maximum upgrade!';
+    }
+}
