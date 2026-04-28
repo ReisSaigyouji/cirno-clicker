45b58f2e7ad9ef45bb3e42dd62aa5c7afaadc319
Reis Saigyouji <olegride22@gmail.com>
2026-04-28 18:27:34 +0200

    made the code for upgrade look better

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var count = 0;
 var power = 1;
-var price = 20;
+var i = 0
 const counter = document.getElementById('counter');
 const clickbtn = document.getElementById('click');
 const upbtn = document.getElementById('upgrade');
 const cost = document.getElementById('cost');
+const prices = [20, 50, 100, 300, 600, 999]
+var price = prices[0];
 
 clickbtn.onclick = function() {
     count += power;
@@ -12,33 +14,19 @@ clickbtn.onclick = function() {
 }
 
 upbtn.onclick = function() {
-    if (count >= price && price === 20) {
+    if (count >= price && i < prices.length) {
         power *= 2;
         count -= price;
-        price = 100;
-        cost.innerText = 'Upgrade cost: 100';
+        counter.innerText = count;
+        i++;
+        if (i < prices.length) {
+            price = prices[i];
+            cost.innerText = `Upgrade cost: ${price}`;
+        } else {
+            cost.innerText = 'Maximal upgrade!';
+        }
     }
-    else if (count >= price && price === 100) {
-        power *= 2;
-        count -= price;
-        price = 300;
-        cost.innerText = 'Upgrade cost: 300';
-    }
-    else if (count >= price && price === 300) {
-        power *= 2;
-        count -= price;
-        price = 600;
-        cost.innerText = 'Upgrade cost: 600';
-    }
-    else if (count >= price && price === 600) {
-        power *= 2;
-        count -= price;
-        price = 999;
-        cost.innerText = 'Upgrade cost: 999';
-    }
-    else if (count >= price && price === 999) {
-        power *= 2;
-        count -= price;
-        cost.innerText = 'Maximum upgrade!';
+    else if (i < prices.length && count < price) {
+        cost.innerText = `Need ${price - count} more coins for next upgrade!`;
     }
 }
