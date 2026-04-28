40d45cbf67b675323058ee68fd05152e370e64b8
Reis Saigyouji <olegride22@gmail.com>
2026-04-28 15:09:58 +0200

    count -= price* I'm dumb

--- index.js
+++ index.js
@@ -14,30 +14,31 @@ clickbtn.onclick = function() {
 upbtn.onclick = function() {
     if (count >= price && price === 20) {
         power *= 2;
-        count -= power;
+        count -= price;
         price = 100;
         cost.innerText = 'Upgrade cost: 100';
     }
     else if (count >= price && price === 100) {
         power *= 2;
-        count -= power;
+        count -= price;
         price = 300;
         cost.innerText = 'Upgrade cost: 300';
     }
     else if (count >= price && price === 300) {
         power *= 2;
-        count -= power;
+        count -= price;
         price = 600;
         cost.innerText = 'Upgrade cost: 600';
     }
     else if (count >= price && price === 600) {
         power *= 2;
-        count -= power;
+        count -= price;
         price = 999;
         cost.innerText = 'Upgrade cost: 999';
     }
     else if (count >= price && price === 999) {
         power *= 2;
+        count -= price;
         cost.innerText = 'Maximum upgrade!';
     }
 }
