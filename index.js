e0ae325993d7351c5eaab7668294bd9f053e32a0
Reis Saigyouji <olegride22@gmail.com>
2026-05-11 00:47:26 +0200

    a little rework, made js code look better, also added reset button, will style it later

--- index.js
+++ index.js
@@ -1,54 +1,151 @@
-var count = 0;
-var power = 1;
-var i = 0
+let count = 0;
+let power = 1;
+let i = 0;
+let stage = 1;
+
 const counter = document.getElementById('counter');
 const clickbtn = document.getElementById('click');
 const upbtn = document.getElementById('upgrade');
 const cost = document.getElementById('cost');
-const prices = [20, 50, 100, 300, 600, 999]
-var price = prices[0];
 const cirno = document.getElementById('cirno');
-var stage = 1
+const resetbtn = document.getElementById('reset');
+
+const prices = [20, 50, 100, 300, 600, 999];
+let price = prices[0];
+
+// load from local storage
+
+if (localStorage.getItem('count')) {
+    count = Number(localStorage.getItem('count'));
+    power = Number(localStorage.getItem('power'));
+    i = Number(localStorage.getItem('i'));
+    price = Number(localStorage.getItem('price'));
+    stage = Number(localStorage.getItem('stage'));
+}
+
+// restore everything for ui if reloaded
+
+if (count >= 9999) {
+    count = 9999;
+    counter.innerText = '9999! You won!';
+    clickbtn.style.display = 'none';
+} else {
+    counter.innerText = count;
+}
+
+if (stage === 2) {
+    cirno.src = './img/c2.png';
+}
+else if (stage >= 3) {
+    cirno.src = './img/c3.png';
+}
+
+if (i >= prices.length) {
+    cost.innerText = 'Max upgrade!';
+    upbtn.style.display = 'none';
+} else {
+    cost.innerText = `Upgrade cost: ${price}`;
+}
+
+// clicker
 
 clickbtn.onclick = function() {
+
     cirno.classList.add('pulse');
+
     count += power;
-    counter.innerText = count;
+
+    // limit
+    if (count >= 9999) {
+        count = 9999;
+    }
+
+    // pics for stages
     if (count >= 99 && stage === 1) {
         cirno.src = './img/c2.png';
-        stage++
+        stage = 2;
     }
     else if (count >= 999 && stage === 2) {
         cirno.src = './img/c3.png';
-        stage++
+        stage = 3;
     }
 
+    // win state
     if (count >= 9999) {
         counter.innerText = '9999! You won!';
         clickbtn.style.display = 'none';
+    } else {
+        counter.innerText = count;
     }
-}
+
+    saveGame();
+};
+
+// pulse reset
 
 cirno.addEventListener('animationend', () => {
     cirno.classList.remove('pulse');
 });
 
+// upgrade
+
 upbtn.onclick = function() {
-    if (count >= price && i < prices.length) {
-        power *= 2;
-        count -= price;
-        counter.innerText = count;
-        i++;
-        if (i < prices.length) {
-            price = prices[i];
-            cost.innerText = `Upgrade cost: ${price}`;
-        } else {
-            cost.innerText = 'Maximal upgrade!';
-            upbtn.style.display = 'none';
-        }
+
+    if (i >= prices.length) {
+        cost.innerText = 'Maximal upgrade!';
+        return;
     }
-    else if (i < prices.length && count < price) {
+
+    if (count < price) {
         cost.innerText = `Need ${price - count} more coins for next upgrade!`;
+        return;
     }
+    power *= 2;
+    count -= price;
+    counter.innerText = count;
+    i++;
+
+    if (i >= prices.length) {
+        cost.innerText = 'Max upgrade!';
+        upbtn.style.display = 'none';
+    } else {
+        price = prices[i];
+        cost.innerText = `Upgrade cost: ${price}`;
+    }
+
+    saveGame();
+};
+
+// save to local storag
+
+function saveGame() {
+    localStorage.setItem('count', count);
+    localStorage.setItem('power', power);
+    localStorage.setItem('i', i);
+    localStorage.setItem('price', price);
+    localStorage.setItem('stage', stage);
 }
 
+resetbtn.onclick = function() {
+    // reset variables
+    count = 0;
+    power = 1;
+    i = 0;
+    stage = 1;
+    price = prices[0];
+
+    // reset text
+    counter.innerText = count;
+    cost.innerText = `Upgrade cost: ${price}`;
+
+    // reset image
+    cirno.src = './img/c1.png';
+
+    // restore buttons
+    clickbtn.style.display = 'inline-block';
+    upbtn.style.display = 'inline-block';
+
+    // clear localStorage
+    localStorage.clear();
+
+}
